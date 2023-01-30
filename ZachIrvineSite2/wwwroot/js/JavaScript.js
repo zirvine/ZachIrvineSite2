164fe15df208489425bb8c8158314317cbe3b71b
irvin <irvin@DESKTOP-R5VJPO9>
2023-01-30 09:51:37 -0700

    Add project files.

---
+++ ZachIrvineSite2/wwwroot/js/JavaScript.js
@@ -0,0 +1,5 @@
+﻿$("#submitScores").click(function () {
+    var totalScore = 0;
+    totalScore = ($("#assignments").val() * 0.5) + ($("#groupProjects").val() * 0.1) + ($("#quizzes").val() * 0.1) + ($("#midterm").val() * 0.1) + ($("#final").val() * 0.1) + ($("#intex").val() * 0.1)
+    $("#results").text("Your total score is " + totalScore + "\%");
+})
