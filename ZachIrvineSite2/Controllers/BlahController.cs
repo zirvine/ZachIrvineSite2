35b7594687d6f053889d31ecbca47d7e3865626b
Zachary Irvine <irvinezachary@gmail.com>
2023-02-22 11:12:22 -0700

    HW5

---
+++ ZachIrvineSite2/Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZachIrvineSite2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZachIrvineSite2.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalc(GradeCalcClass model)
+        {
+            return View();
+        }
+    }
+}
---
+++ ZachIrvineSite2/Models/GradeCalcClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZachIrvineSite2.Models
+{
+    public class GradeCalcClass
+    {
+        [Range(0, 1000, ErrorMessage = "Enter number between 0 to 1000")]
+        public int Assignments { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Enter number between 0 to 1000")]
+        public int GroupProjects { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Enter number between 0 to 1000")]
+        public int Quizzes { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Enter number between 0 to 1000")]
+        public int Midterm { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Enter number between 0 to 1000")]
+        public int Final { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Enter number between 0 to 1000")]
+        public int Intex { get; set; }
+    }
+}
